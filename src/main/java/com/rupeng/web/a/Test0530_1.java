6f4290e47b0cb918d2ccfd7054b4b9a20dbf9b1d
wch <840785157@qq.com>
2021-05-30 23:31:16 +0800

    第一次整体提交

---
+++ src/main/java/com/rupeng/web/a/Test0530_1.java
@@ -0,0 +1,12 @@
+package com.rupeng.web.a;
+
+/**
+ * @author shkstart
+ * @create 2021-05-30 18:33
+ */
+public class Test0530_1 {
+    public static void main(String[] args) {
+        System.out.println("第1次提交并上传");
+    }
+
+}
---
+++ src/main/java/com/rupeng/web/a/Test0530_2.java
@@ -0,0 +1,11 @@
+package com.rupeng.web.a;
+
+/**
+ * @author shkstart
+ * @create 2021-05-30 18:33
+ */
+public class Test0530_2 {
+    public static void main(String[] args) {
+        System.out.println("第1次提交并上传");
+    }
+}
